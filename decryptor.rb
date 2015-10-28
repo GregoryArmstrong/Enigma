705a80b5dc16f4f17e452c2bce571a25eddb514a
GregoryArmstrong <intergalacticgentleman@gmail.com>
2015-10-28 13:13:49 -0600

    functional encryptor, decryptor, keygenerator.

--- decryptor.rb
+++ decryptor.rb
@@ -1,44 +1,61 @@
-require_relative 'enigma'
-require_relative 'key_generator'
+require './key_generator'
+require './enigma'
 
-class Encryptor
 
-  attr_accessor :message, :key, :encrypted_message, :wheels
+class Decryptor
+
+  attr_accessor :message, :key, :decrypted_message, :wheels
 
   def initialize(message=nil, key=nil)
     @message = message
-    @key = KeyGenerator.new(12345)
-    @encrypted_message = []
+    @key = KeyGenerator.new
+    @decrypted_message = []
     @wheels = []
   end
 
-  def encryption_key
+  def decryption_key
     encryption_key = ('a'..'z').to_a + ("0".."9").to_a + [" ", ".", ","]             # => ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ", ".", ","]
+    decryption_key = encryption_key.reverse!
   end
 
   def find_rotation(letter)
-    stored_index = encryption_key.find_index(letter)
+    stored_index = decryption_key.find_index(letter)
     total_rotation = ((@wheels[0] + stored_index) % 39)
   end
 
   def change_letter(letter)
-    stored_letter = encryption_key.fetch(find_rotation(letter))
+    stored_letter = decryption_key.fetch(find_rotation(letter))
   end
 
-  def encrypt_letter(letter)
-    @encrypted_message << change_letter(letter)
+  def decrypt_letter(letter)
+    @decrypted_message << change_letter(letter)
   end
 
-  def encrypt_message
+  def decrypt_message
     assign_wheels
     @message.chars.each do |letter|
-      encrypt_letter(letter)
+      decrypt_letter(letter)
       @wheels.rotate!
     end
-    @encrypted_message = @encrypted_message.join("")
+    @decrypted_message = @decrypted_message.join("")
   end
 
   def assign_wheels
       @wheels = [@key.a, @key.b, @key.c, @key.d]
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
---
+++ decryptor_test.rb
@@ -0,0 +1,28 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './decryptor'
+
+class DecryptorTest < Minitest::Test
+
+  def test_can_create_instance_of_decryptor
+    d = Decryptor.new("4fotz5", 12345)
+
+    assert d
+  end
+
+  def test_can_decrypt_message_given_correct_key
+    d = Decryptor.new("4fotz5", 12345)
+
+    assert_equal "string", d.decrypt_message
+  end
+
+
+
+
+
+
+
+
+end
